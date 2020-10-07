2df0c6c335ab0149c794d67fbbd4ff9fc6a19463
Rzeszowski17 <70712364+Rzeszowski17@users.noreply.github.com>
2020-10-07 21:43:25 +0200

    Add files via upload

---
+++ 3_dolaczenie_plikow.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+Początek plik
+<hr>
+<?php //Dołączenie plik php
+	include './Do_4.php';
+	include_once './Do_4.php';
+	
+	require './Do_4.php';
+	require_once './Do_4.php';
+?>
+<hr>
+Koniec pliku
+</body>
+</html>
