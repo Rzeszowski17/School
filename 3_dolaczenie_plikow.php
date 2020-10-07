d30252d0f30992fe197b0c491345feca003bb754
Rzeszowski17 <70712364+Rzeszowski17@users.noreply.github.com>
2020-10-07 22:00:16 +0200

    Update 3_dolaczenie_plikow.php

--- 3_dolaczenie_plikow.php
+++ 3_dolaczenie_plikow.php
@@ -5,11 +5,13 @@
 Początek plik
 <hr>
 <?php //Dołączenie plik php
+
 	echo '<br>include<br>';
-	include './Do_3.php';
-	include_once './Do_3.php';
+	include './Do_3.php'; // wyswirtla dalsza czesc strony pomimo bledu
+	include_once './Do_3.php'; //powoduje ze plik wiecej razy sie nie otworzy
+	echo '<br>';
 	echo '<br>require<br>';
-	require './Do_3.php';
+	require './Do_3.php'; // powoduje fatal error i nie wyswietla dalszej czesci strony
 	require_once './Do_3.php';
 ?>
 <hr>
