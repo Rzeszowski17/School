15304f1df04078b46196ba9ac8dccb3fa5685971
Rzeszowski17 <70712364+Rzeszowski17@users.noreply.github.com>
2020-11-01 19:28:19 +0100

    Add files via upload

--- Zadania/Kalkulator/Kalkulator.php
+++ Zadania/Kalkulator/Kalkulator.php
@@ -17,11 +17,42 @@ table{
     </style>
 </head>
 <body>
+
+  <?php
+
+      require_once './function.php';
+
+      if(isset($_POST['submit']))
+    	{
+      if(is_numeric($_POST['x']) && is_numeric($_POST['y']))
+     {
+       $x = $_POST['x'];
+       $y = $_POST['y'];
+
+            switch($_POST["sign"]){
+               case 'add':
+               $result=add($x,$y);
+               break;
+               case 'sub':
+               $result=sub($x,$y);
+               break;
+               case 'mul':
+               $result=mul($x,$y);
+               break;
+               case 'div':
+               $result=div($x,$y);
+               break;
+               default:
+             }
+           }
+         }
+  ?>
+
     <table>
     <tr><td><div class="baner">Kalkulator</div></td></tr>
-    <form method="POST">
+    <form  method="POST">
     <tr>
-    <td><input type="number" name="x" placeholder="x"></td>
+    <td><input type="number" name="x"  placeholder="x" pattern="[0-9]"></td>
     <td>
     <select name="sign">
     <option value="add">+</option>
@@ -30,53 +61,13 @@ table{
     <option value="div">/</option>
     </select>
     </td>
-    <td><input type="number" name="y" placeholder="y" ></td>
+    <td><input type="number" name="y" placeholder="y" pattern="[0-9]"></td>
     <td><input type="submit" name="submit" value="="> </td>
-    <td><input type="number"  name="result" disabled value="<?php if(isset ($result)){echo $result;}?>"></td>
-    </tr>
+    <td><input type="number" name="result" disabled value= "<?php if(isset ($result)){echo $result;}?>"></td>
+
+  </tr>
     </form>
     </table>
-
-<?php
-    require_once './function.php';
-
-    if(isset($_POST['submit'])) 
-	{
-
-    if(is_numeric($_POST['x']) && is_numeric($_POST['y'])) 
-    {
-    $x=$_POST['x'];
-    $y=$_POST['y'];
-    
-    switch($_POST['sign']){
-
-        case'add':
-            $result = add($x, $y);
-    break;
-
-        case'sub':
-            $result = sub($x, $y);
-    break;
-
-        case'mul':
-            $result = mul($x, $y);
-    break;
-
-        case'div':
-            $result = div($x, $y);
-    break;
-
-    default:
-            $result = "No option";
-	break;
-    }
-  }  
-}
-    
-?>
- 
-
-
 
 </body>
 </html>
