361fdc72883392338b517bf7c2adc4265bebc385
Rzeszowski17 <70712364+Rzeszowski17@users.noreply.github.com>
2020-11-07 21:56:47 +0100

    Add files via upload

--- Zadania/05.11.20/prostopadloscian.php
+++ Zadania/05.11.20/prostopadloscian.php
@@ -20,6 +20,7 @@ hr{
 </head>
 <body>
     <table>
+    <form method="POST">
     <tr>
     <td><div class="baner">Prostopadloscian<div></td>
     </tr>
@@ -46,6 +47,41 @@ hr{
     <tr>
     <td><input type="submit" name="oblicz" value="Oblicz"></td>
     </tr>
+    <tr>
+    <td>
+    <?php  
+    echo prost()
+    ?>
+    </td>
+    </tr>
+    <tr>
+    <td><a href="3_zadanie_formularz_geometria.php">Powrót do strony głównej</a></td>
+    </tr>
+    </form>
     </table>
+    <?php
+
+    function prost(){
+    if (isset($_POST['a'],$_POST['b'], $_POST['c'])){
+         if($_POST['a']<=0||$_POST['b']<=0||$_POST['c']<=0){
+            echo "<br><h4>Liczby podane w polach powinny być dodatnei</h4>";
+        }
+        else{
+            require_once('./scripts/prostopadloscian.php');
+
+
+        echo '<ul>';
+        echo '<li>Pole:'.Pole_Prosto($a,$b,$c).'cm<sup>2</sup></li>';
+        echo '<li>Objętość:'.Obje_Prosto($a,$b,$c).'cm<sup>3</sup></li>';
+        echo '<li>Długość przekątnej:'.Dlugo_Prosto($a,$b,$c).'cm</li>';
+        echo '</ul>';
+
+
+        }
+    }
+
+    }
+
+    ?>
 </body>
 </html>
